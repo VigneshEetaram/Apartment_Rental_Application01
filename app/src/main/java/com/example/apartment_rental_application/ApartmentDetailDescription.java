7de1a7682c7eda8c30a3b161dab21c185c5d3150
Husanjeet <husanjeetkaur1996@gmail.com>
2021-01-31 11:20:59 -0500

    initial commit

---
+++ app/src/main/java/com/example/apartment_rental_application/ApartmentDetailDescription.java
@@ -0,0 +1,14 @@
+package com.example.apartment_rental_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ApartmentDetailDescription extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apartment_detail_description);
+    }
+}
