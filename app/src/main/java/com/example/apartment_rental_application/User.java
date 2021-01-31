b3c24ddb42fa53d319b14ecc4d0b12c4b1e5dc66
vignesheetaram <vignesh.eetaram@gmail.com>
2021-01-31 00:42:50 -0500

    Add Sign in

---
+++ app/src/main/java/com/example/apartment_rental_application/User.java
@@ -0,0 +1,14 @@
+package com.example.apartment_rental_application;
+
+public class User {
+    public String name,age,email;
+
+    public User() {
+
+    }
+    public User(String name, String age, String email) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+}
