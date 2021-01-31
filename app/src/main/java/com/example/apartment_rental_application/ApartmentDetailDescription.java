dd26c05c9c4eae2c738c011181a1c8f737e828dd
Husanjeet <husanjeetkaur1996@gmail.com>
2021-01-31 12:06:01 -0500

    apartment details description

--- app/src/main/java/com/example/apartment_rental_application/ApartmentDetailDescription.java
+++ app/src/main/java/com/example/apartment_rental_application/ApartmentDetailDescription.java
@@ -3,9 +3,16 @@ package com.example.apartment_rental_application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ApartmentDetailDescription extends AppCompatActivity {
 
+    private TextView Price,Description,Title;
+    FloatingActionButton button;
+    ImageButton share;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
