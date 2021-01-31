ff7622a281c96f4ab270df2bcadfa2b35ef6233f
vignesheetaram <vignesh.eetaram@gmail.com>
2021-01-30 23:23:53 -0500

    add test comment

--- app/src/main/java/com/example/apartment_rental_application/Renterlogin.java
+++ app/src/main/java/com/example/apartment_rental_application/Renterlogin.java
@@ -3,6 +3,10 @@ package com.example.apartment_rental_application;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ *
+ * Renter Login Main function
+ */
 public class Renterlogin extends AppCompatActivity {
 
     @Override
