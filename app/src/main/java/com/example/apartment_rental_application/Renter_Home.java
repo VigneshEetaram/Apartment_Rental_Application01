54ece4e79dfcf30958016d01f81ba7b1ec539e76
vignesheetaram <vignesh.eetaram@gmail.com>
2021-02-01 02:37:08 -0500

    Renter Start

---
+++ app/src/main/java/com/example/apartment_rental_application/Renter_Home.java
@@ -0,0 +1,14 @@
+package com.example.apartment_rental_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Renter_Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_renter__home);
+    }
+}
