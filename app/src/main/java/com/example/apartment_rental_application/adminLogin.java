896957930a9d654b09053b6c9bcf0ea62002d990
jg52can <jobingrg52@gmail.com>
2021-02-01 03:39:42 -0500

    view ads

---
+++ app/src/main/java/com/example/apartment_rental_application/adminLogin.java
@@ -0,0 +1,71 @@
+package com.example.apartment_rental_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+public class adminLogin extends AppCompatActivity {
+    EditText admEmail,admPass;
+    Button adminLogin;
+    String email;
+    String password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_login);
+
+        admEmail = (EditText)findViewById(R.id.txtAdminEmail);
+        admPass = (EditText)findViewById(R.id.txtAdminPassword);
+        adminLogin = (Button)findViewById(R.id.btnAdminLogin);
+
+        adminLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkConditions();
+            }
+        });
+    }
+
+    private void checkConditions() {
+        email = admEmail.getText().toString();
+        password = admPass.getText().toString();
+
+        if(email.isEmpty()){
+            admEmail.setError("Email is required");
+            admEmail.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            admEmail.setError("Provide valid email");
+            admEmail.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()){
+            admPass.setError("Password is required");
+            admPass.requestFocus();
+            return;
+        }
+        checkLogin();
+    }
+
+    private void checkLogin() {
+        if(email.equals("admin@gmail.com") &&
+                password.equals("admin")){
+            startActivity(new Intent(adminLogin.this,adminHome.class));
+        }
+        else{
+            Toast.makeText(adminLogin.this, "Failed to login! Try again.", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+}
