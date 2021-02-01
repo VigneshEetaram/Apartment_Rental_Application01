d8fea485c68eb8788a34836c4130042ee9adf3f1
vignesheetaram <vignesh.eetaram@gmail.com>
2021-02-01 03:27:36 -0500

    Renter post design

---
+++ app/src/main/java/com/example/apartment_rental_application/Post_Apartment_Page.java
@@ -0,0 +1,20 @@
+package com.example.apartment_rental_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class Post_Apartment_Page extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post__apartment__page);
+
+
+    }
+}
